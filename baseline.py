ceb89373d961ca73aa90cc7175e2d2b77ad284e6
Ivan Ermakov <ermakov.ia@phystech.edu>
2022-09-17 21:55:37 +0300

    Increase the problem size

--- baseline.py
+++ baseline.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-A = np.ones((10000, 10000, 3), dtype = np.float32)
+A = np.ones((20000, 20000, 3), dtype = np.float32)
 B = np.ones((3, 3), dtype = np.float32)
 
 C = A @ B
--- my.py
+++ my.py
@@ -1,7 +1,7 @@
 import numpy as np
 import fastmul
 
-A = np.ones((10000, 10000, 3), dtype = np.float32)
+A = np.ones((20000, 20000, 3), dtype = np.float32)
 B = np.ones((3, 3), dtype = np.float32)
 
 C = fastmul.dot(A, B)
